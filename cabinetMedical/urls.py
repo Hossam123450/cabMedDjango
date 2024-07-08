8fc8342de7b3f14db064fea3b45fc02c4c1baaab
Hossam Eddine Chahi <hossamchahi77@gmail.com>
2024-07-08 10:39:56 +0100

    a 7na kan9ado flUX UI

--- cabinetMedical/urls.py
+++ cabinetMedical/urls.py
@@ -1,4 +1,13 @@
 from . import views
 from django.urls import path
 app_name='cabinetMedical'
-urlpatterns = [ path('cab',views.cab,name='cab'),]
+urlpatterns = [
+               path('cab/',views.cab,name='cab'), 
+               path('cab1/',views.cab1,name='cab1'),
+               path('cab2/',views.cab2,name='cab2'),
+               path('cab3/',views.cab3,name='cab3'),
+               path('cab4/',views.cab4,name='cab4')
+            
+            ]
+               
+
--- cabinetMedical/views.py
+++ cabinetMedical/views.py
@@ -11,95 +11,94 @@ from cabinetMedical.models import Modifrdv,Supprdv,Rdv
 # from django.http import HttpResponse
 # Create your views here.
 
-def cab(request):
-    form=Inscription()
-    form2=Connexion()
-    form3=ContactForm()
-    form4=RdvForm()
-    form5=ModifrdvForm()
-    form6=SupprdvForm()
-    x=0
-    # if request.user.is_authenticated:
-    #     x=1
-    nom5= 'xxx'
-    # user1=User()
-    message = ''
-    if request.method == 'POST':
-        form2 = Connexion(request.POST)
-        if form2.is_valid():
-            user = authenticate(
-                username=form2.cleaned_data['nom'],
-                password=form2.cleaned_data['motDePasse'],
-            )
-            if user is not None:
-                login(request, user)
-                x=1
-                message = f'Bonjour, {user.username}! Vous êtes connecté.'
-            else:
-                message = 'Identifiants invalides.'
+# def cab(request):
+#     form=Inscription()
+#     form2=Connexion()
+#     form3=ContactForm()
+#     form4=RdvForm()
+#     form5=ModifrdvForm()
+#     form6=SupprdvForm()
+#     # if request.user.is_authenticated:
+#     #     x=1
+#     nom5= 'xxx'
+#     # user1=User()
+#     message = ''
+#     if request.method == 'POST':
+#         form2 = Connexion(request.POST)
+#         if form2.is_valid():
+#             user = authenticate(
+#                 username=form2.cleaned_data['nom'],
+#                 password=form2.cleaned_data['motDePasse'],
+#             )
+#             if user is not None:
+#                 login(request, user)
+#                 x=1
+#                 message = f'Bonjour, {user.username}! Vous êtes connecté.'
+#             else:
+#                 message = 'Identifiants invalides.'
             
-        form = Inscription(request.POST)
-        if form.is_valid():
-            form.save()		
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request,user)	
-            x=1
-            message = f'Bonjour, {user.username}! Vous êtes connecté.'
-            return redirect('/cabinetMedical/cab')
+#         form = Inscription(request.POST)
+#         if form.is_valid():
+#             form.save()		
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password1']
+#             user = authenticate(username=username, password=password)
+#             login(request,user)	
+#             x=1
+#             message = f'Bonjour, {user.username}! Vous êtes connecté.'
+#             return redirect('/cabinetMedical/cab')
          
-        form3=ContactForm(request.POST)
-        if form3.is_valid():
-            form3.save()
-            x=1
-            return redirect('/cabinetMedical/cab')
-        form4=RdvForm(request.POST)
-        if form4.is_valid():
-            nom=form4.cleaned_data['nom2']
-            email=form4.cleaned_data['email2']
-            tel1=form4.cleaned_data['tel']
-            date1=form4.cleaned_data['date']
-            message=form4.cleaned_data['message2']
-            rdv=Rdv(user=request.user,nom2=nom,email2=email,tel=tel1,date=date1,message2=message)
-            rdv.save()
-            x=1
-            return redirect('/cabinetMedical/cab')
-        form5=ModifrdvForm(request.POST)
-        if form5.is_valid():
-            nom=form5.cleaned_data['nom3']
-            email=form5.cleaned_data['email3']
-            tel1=form5.cleaned_data['tel1']
-            date1=form5.cleaned_data['date1']
-            message=form5.cleaned_data['message3']
-            up=Rdv.objects.get(user=request.user)
-            up.delete()
-            b=Rdv(user=request.user,nom2=nom,email2=email,tel=tel1,date=date1,message2=message)
-            b.save()
-            x=1
-            return redirect('/cabinetMedical/cab')
-        form6=SupprdvForm(request.POST)
-        if form6.is_valid():
-            nom=form6.cleaned_data['nom4']
-            delet=Rdv.objects.get(user=request.user)
-            delet.delete()
-            x=1
-            return redirect('/cabinetMedical/cab')
+#         form3=ContactForm(request.POST)
+#         if form3.is_valid():
+#             form3.save()
+#             x=1
+#             return redirect('/cabinetMedical/cab')
+#         form4=RdvForm(request.POST)
+#         if form4.is_valid():
+#             nom=form4.cleaned_data['nom2']
+#             email=form4.cleaned_data['email2']
+#             tel1=form4.cleaned_data['tel']
+#             date1=form4.cleaned_data['date']
+#             message=form4.cleaned_data['message2']
+#             rdv=Rdv(user=request.user,nom2=nom,email2=email,tel=tel1,date=date1,message2=message)
+#             rdv.save()
+#             x=1
+#             return redirect('/cabinetMedical/cab')
+#         form5=ModifrdvForm(request.POST)
+#         if form5.is_valid():
+#             nom=form5.cleaned_data['nom3']
+#             email=form5.cleaned_data['email3']
+#             tel1=form5.cleaned_data['tel1']
+#             date1=form5.cleaned_data['date1']
+#             message=form5.cleaned_data['message3']
+#             up=Rdv.objects.get(user=request.user)
+#             up.delete()
+#             b=Rdv(user=request.user,nom2=nom,email2=email,tel=tel1,date=date1,message2=message)
+#             b.save()
+#             x=1
+#             return redirect('/cabinetMedical/cab')
+#         form6=SupprdvForm(request.POST)
+#         if form6.is_valid():
+#             nom=form6.cleaned_data['nom4']
+#             delet=Rdv.objects.get(user=request.user)
+#             delet.delete()
+#             x=1
+#             return redirect('/cabinetMedical/cab')
 
-    contexte={
-          "form":form,
-          "form2":form2,
-          "form3":form3,
-          "form4":form4,
-          "form5":form5,
-          "form6":form6,
-          "message":message,
-          "x":x,
-          "nom5":nom5,
-        #   "user1":user1,
+#     contexte={
+#           "form":form,
+#           "form2":form2,
+#           "form3":form3,
+#           "form4":form4,
+#           "form5":form5,
+#           "form6":form6,
+#           "message":message,
+#           "x":x,
+#           "nom5":nom5,
+#         #   "user1":user1,
         
-    }
-    return render(request,'index.html',contexte)
+#     }
+#     return render(request,'index1.html',contexte)
     #  return render(request,'index.html',{}) 
     # user=form.save()
     
@@ -112,6 +111,87 @@ def cab(request):
     #         user = authenticate(username=username, password=password)
     #         login(request,user)	
     #         return redirect(settings.LOGIN_REDIRECT_URL)
+def cab(request):
+    return render(request,'indexX.html')
 
+def cab2(request):
+    form2=Connexion()
+    nom5= 'xxx'
+    message = ''
+    if request.method == 'POST':
+        form2 = Connexion(request.POST)
+        if form2.is_valid():
+            user = authenticate(
+                username=form2.cleaned_data['nom'],
+                password=form2.cleaned_data['motDePasse'],
+            )
+            if user is not None:
+                login(request, user)
+                message = f'Bonjour, {user.username}! Vous êtes connecté.'
+            else:
+                message = 'Identifiants invalides.'
+    contexte={
+          "form2":form2,
+          "message":message,
+          "nom5":nom5,   
+    }
+    return render(request,'index2.html',contexte)
+
+
+def cab1(request):
+    form=Inscription()
+    nom5= 'xxx'
+    message = ''
+    if request.method == 'POST':
+        form = Inscription(request.POST)
+        if form.is_valid():
+            form.save()		
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request,user)	
+            message = f'Bonjour, {user.username}! Vous êtes connecté.'
+    contexte={
+          "form":form,
+          "message":message,
+          "nom5":nom5,        
+    }
+    return render(request,'index1.html',contexte)
+
+
+
+def cab3(request):
+    form3=ContactForm()
+    if request.method == 'POST': 
+        form3=ContactForm(request.POST)
+        if form3.is_valid():
+            form3.save()
+    contexte={
+          "form3":form3,
+        
+    }
+    return render(request,'index3.html',contexte)
+
+
+
+
+def cab4(request):
+    form4=RdvForm()
+    if request.method == 'POST':
+        form4=RdvForm(request.POST)
+        if form4.is_valid():
+            nom=form4.cleaned_data['nom2']
+            email=form4.cleaned_data['email2']
+            tel1=form4.cleaned_data['tel']
+            date1=form4.cleaned_data['date']
+            message=form4.cleaned_data['message2']
+            rdv=Rdv(user=request.user,nom2=nom,email2=email,tel=tel1,date=date1,message2=message)
+            rdv.save()
+    contexte={
+          "form4":form4,
+          "message":message,
+
+    }
+    return render(request,'index4.html',contexte)
 
 
